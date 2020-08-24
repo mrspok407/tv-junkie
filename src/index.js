87ec138e58b253553f25e0b6f5211b390049a1f6
mrspok407 <mr.spok407@gmail.com>
2020-08-24 20:50:51 +0300

    IE11 support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import "react-app-polyfill/ie11"
+import "react-app-polyfill/stable"
 import React from "react"
 import ReactDOM from "react-dom"
 import "normalize.css"
-import "./index.scss"
 import App from "./App"
 import Firebase, { FirebaseContext } from "./Components/Firebase"
+import "./index.scss"
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
