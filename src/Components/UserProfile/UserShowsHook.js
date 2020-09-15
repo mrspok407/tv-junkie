4757171e4afa4d3be0c7141a49799bb20d82b0e8
mrspok407 <mr.spok407@gmail.com>
2020-09-15 17:43:32 +0300

    test

--- src/Components/UserProfile/UserShowsHook.js
+++ src/Components/UserProfile/UserShowsHook.js
@@ -13,81 +13,54 @@ const useUserShows = firebase => {
     console.log("userShowsHook mounted")
     authUserListener()
     return () => {
+      console.log("test222")
       authUserListener()
     }
   }, [])
 
   useEffect(() => {
     sessionStorage.setItem(SESSION_STORAGE_KEY_SHOWS, JSON.stringify(userShows))
-    console.log("comp updated")
   }, [userShows])
 
   const authUserListener = () => {
     firebase.onAuthUserListener(
       authUser => {
-        console.log("function run")
         firebase.userAllShows(authUser.uid).on("value", snapshot => {
           if (snapshot.val() === null) {
             setLoadingShows(false)
             return
           }
 
-          console.log("listener on")
+          const userShowsSessionStorage = JSON.parse(sessionStorage.getItem(SESSION_STORAGE_KEY_SHOWS))
+
+          console.log(userShowsSessionStorage)
+          console.log(snapshot.val())
 
           const shows = Object.values(snapshot.val()).map(show => {
             return show
           })
-          const userShowsSS = JSON.parse(sessionStorage.getItem(SESSION_STORAGE_KEY_SHOWS))
-          const userShowsSSLength = userShowsSS.length
 
-          if (userShowsSSLength === 0) {
-            Promise.all(
-              Object.values(snapshot.val()).map(show => {
-                return firebase
-                  .showInfo(show.status, show.id)
-                  .once("value")
-                  .then(snapshot => {
-                    return {
-                      ...show,
-                      ...snapshot.val()
-                    }
-                  })
-              })
-            ).then(showsDatabase => {
-              const mergedShows = merge(shows, showsDatabase, {
-                arrayMerge: combineMergeObjects
-              })
-
-              console.log("length 0")
-              setUserShows(mergedShows)
-              setLoadingShows(false)
+          Promise.all(
+            Object.values(snapshot.val()).map(show => {
+              return firebase
+                .showInfo(show.status, show.id)
+                .once("value")
+                .then(snapshot => {
+                  return {
+                    ...show,
+                    ...snapshot.val()
+                  }
+                })
             })
-          } else if (userShowsSSLength < shows.length) {
-            shows.forEach((show, index) => {
-              if (userShowsSS.find(item => item.id === show.id)) return
-
-              console.log("length less")
-
-              firebase.showInfo(show.status, show.id).once("value", snapshot => {
-                const mergedShow = { ...show, ...snapshot.val() }
-                const updatedShows = userShowsSS.splice(index, 0, mergedShow)
-
-                console.log(updatedShows)
-
-                setUserShows(updatedShows)
-                setLoadingShows(false)
-              })
-            })
-          } else if (userShowsSSLength === shows.length) {
-            const mergedShows = merge(userShowsSS, shows, {
+          ).then(showsDatabase => {
+            const mergedShows = merge(shows, showsDatabase, {
               arrayMerge: combineMergeObjects
             })
 
-            console.log("length equal")
-
+            console.log("updated userContent")
             setUserShows(mergedShows)
             setLoadingShows(false)
-          }
+          })
         })
 
         // firebase.userAllShows(authUser.uid).once("value", snapshot => {
