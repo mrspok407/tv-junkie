fad286d59811fb09bf1096e56c1634d661b8b9ac
mrspok407 <mr.spok407@gmail.com>
2020-12-27 17:58:14 +0300

    Test of prod/dev modes

--- src/Components/Firebase/Firebase.tsx
+++ src/Components/Firebase/Firebase.tsx
@@ -16,6 +16,26 @@ const config = {
   appId: process.env.REACT_APP_APP_ID
 }
 
+const configDevelopment = {
+  apiKey: process.env.REACT_APP_DEV_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+  measurementId: process.env.REACT_APP_DEV_MEASUREMENT_ID,
+  appId: process.env.REACT_APP_DEV_APP_ID
+}
+
+const test =
+  process.env.NODE_ENV === "production"
+    ? "App is running in production mode"
+    : process.env.NODE_ENV === "development"
+    ? "App is running in development mode"
+    : "lol"
+
+console.log(test)
+
 interface ReferenceInterface {
   uid: string
   key: string
