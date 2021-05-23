285fd817a55bf742991f26c37078ad8cbe962eca
mrspok407 <mr.spok407@gmail.com>
2021-05-23 21:20:49 +0300

    Correct behaviour on line break

--- src/Components/Pages/Contacts/Components/ChatWindow/Components/Input/MessageInput.tsx
+++ src/Components/Pages/Contacts/Components/ChatWindow/Components/Input/MessageInput.tsx
@@ -1,3 +1,4 @@
+import { randomBytes } from "crypto"
 import React, { useState, useEffect, useRef } from "react"
 import "./MessageInput.scss"
 
@@ -13,13 +14,38 @@ const MessageInput: React.FC<Props> = ({}) => {
     inputRef.current.focus()
   }, [])
 
-  const onClick = () => {
-    var sel = window.getSelection()
+  const onClick = (e: any) => {
+    // const inputRef = document.querySelector(".chat-window__input-message") as Node
+    // console.log(inputRef.childNodes)
+
+    const sel = window.getSelection()
     anchorOffset.current = sel?.anchorOffset!
-    console.log(sel?.anchorOffset)
+
+    // console.log(e.target.childNodes)
+
+    // console.log({ anchorOffset: sel?.anchorOffset })
+    // console.log({ focusOffset: sel?.focusOffset })
+    // console.log({ collapsed: sel?.isCollapsed })
+    // console.log({ rangeCount: sel?.rangeCount })
+    // console.log({ anchorNode: sel?.anchorNode?.parentNode?.childNodes[0] })
+
+    // if (sel?.rangeCount! > 0) {
+    //   sel?.removeAllRanges()
+    // }
+
+    // let range: any = document.createRange()
+    // range.setStart(e.target.childNodes[0], 2)
+    // range.setEnd(e.target.childNodes[0], 4)
+
+    // sel?.addRange(range)
   }
 
   const handleOnChange = (e: any) => {
+    const textContent = e.currentTarget.textContent
+    if (textContent === "") {
+      e.currentTarget.innerHTML = ""
+    }
+
     console.log(e.currentTarget.innerHTML)
     setMessage(e.currentTarget.innerHTML)
   }
@@ -27,6 +53,9 @@ const MessageInput: React.FC<Props> = ({}) => {
   const map: any = {}
   const handleKeyDown = (e: any) => {
     console.log(e.key)
+    const sel = window.getSelection()
+    anchorOffset.current = sel?.anchorOffset!
+
     map[e.key] = e.type === "keydown"
     if (e.key === "Enter") {
       console.log("enter pressed")
@@ -37,19 +66,22 @@ const MessageInput: React.FC<Props> = ({}) => {
       //   const div = document.createElement("div")
       //   const br = document.createElement("br")
       //   div.appendChild(br)
-      console.log(message)
       const innerHtml = e.currentTarget.innerHTML
-      console.log(innerHtml)
-      e.currentTarget.innerHTML = `${innerHtml[anchorOffset.current - 1]}\n`
 
-      var range = document.createRange()
-      var sel = window.getSelection()
-      console.log(anchorOffset.current)
-      range.setStart(e.currentTarget.childNodes[0], anchorOffset.current - 1)
-      range.collapse(true)
+      console.log(innerHtml.includes("<br>"))
+      if (!innerHtml.includes("<br>")) {
+        e.currentTarget.innerHTML = `${innerHtml}<br><br>`
+      } else {
+        e.currentTarget.innerHTML = `${innerHtml}<br>`
+      }
 
-      sel?.removeAllRanges()
-      sel?.addRange(range)
+      // const range = document.createRange()
+      // console.log(anchorOffset.current)
+      // range.setStart(e.currentTarget.childNodes[0], anchorOffset.current - 1)
+      // range.collapse(true)
+
+      // sel?.removeAllRanges()
+      // sel?.addRange(range)
 
       setMessage((prevState) => `${prevState}\n`)
 
@@ -80,7 +112,6 @@ const MessageInput: React.FC<Props> = ({}) => {
     <div className="chat-window__input-wrapper">
       <div className="chat-window__input-message-container">
         <div
-          id="test"
           ref={inputRef}
           onClick={onClick}
           onKeyDown={handleKeyDown}
@@ -90,6 +121,7 @@ const MessageInput: React.FC<Props> = ({}) => {
           contentEditable="true"
           dir="auto"
           data-placeholder="Message"
+          suppressContentEditableWarning={true}
         ></div>
       </div>
     </div>
