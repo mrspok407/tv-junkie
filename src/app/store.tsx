3639d275a69798364f148be4c2f6ebb729511104
Ruslan Pivovarov <mr.spok407@gmail.com>
2022-09-04 15:45:17 +0300

    Remove redux dev tools from production

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -14,6 +14,7 @@ export const store = configureStore({
       authUser: JSON.parse(localStorage.getItem('authUser')!) || authUserInitialState.authUser,
     },
   },
+  devTools: process.env.NODE_ENV !== 'production',
 })
 
 export type AppDispatch = typeof store.dispatch
